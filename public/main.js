06547a7815cfe7f62484cf9085e572f58dba73a5
linuxguy467 <me@matthem.codes>
2021-11-09 19:32:48 -0500

    small improvements and add api key

--- public/main.js
+++ public/main.js
@@ -1,52 +1,57 @@
-const weatherDisplay = document.querySelector('.weather')
-const weatherForm = document.querySelector('#weather-form')
-const cityInput = document.querySelector('#city-input')
+const weatherDisplay = document.querySelector('.weather');
+const weatherForm = document.querySelector('#weather-form');
+const cityInput = document.querySelector('#city-input');
 
 // Fetch weather data from API
 const fetchWeather = async (city) => {
-  const url = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=35d31085600cb2d25525ad4855215e03
-`
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=859e7e6266734e0ef466ebd9b62d1b8c
+`;
 
-  const res = await fetch(url)
-  const data = await res.json()
+  const res = await fetch(url);
+  const data = await res.json();
 
   if (data.cod === '404') {
-    alert('City not found')
-    return
+    alert('City not found');
+    return;
   }
 
   const displayData = {
     city: data.name,
     temp: kelvinToFahrenheit(data.main.temp),
-  }
+  };
 
-  addWeatherToDOM(displayData)
-}
+  addWeatherToDOM(displayData);
+};
 
 // Add display data to DOM
 const addWeatherToDOM = (data) => {
-  weatherDisplay.innerHTML = `
-    <h1>Weather in ${data.city}</h1>
-    <h2>${data.temp} &deg;F</h2>
-  `
-  cityInput.value = ''
-}
+  const h1 = document.createElement('h1');
+  h1.textContent = `Weather in ${data.city}`;
+
+  const h2 = document.createElement('h2');
+  h2.innerHTML = `${data.temp} &deg;F`;
+
+  weatherDisplay.appendChild(h1);
+  weatherDisplay.appendChild(h2);
+
+  cityInput.value = '';
+};
 
 // Convert Kelvin to Fahrenheit
 const kelvinToFahrenheit = (temp) => {
-  return Math.ceil(((temp - 273.15) * 9) / 5 + 32)
-}
+  return Math.ceil(((temp - 273.15) * 9) / 5 + 32);
+};
 
 // Event listener for form submission
 weatherForm.addEventListener('submit', (e) => {
-  e.preventDefault()
+  e.preventDefault();
 
   if (cityInput.value === '') {
-    alert('Please enter a city')
+    alert('Please enter a city');
   } else {
-    fetchWeather(cityInput.value)
+    fetchWeather(cityInput.value);
   }
-})
+});
 
 // Initial fetch
-fetchWeather('Miami')
+fetchWeather('Miami');
