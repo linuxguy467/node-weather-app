a724c16e2ef3ac9abcbc408e1c1c3b36d26073cb
linuxguy467 <me@matthem.codes>
2021-11-22 13:12:31 -0500

    remove apicache

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,16 +2,14 @@ const url = require('url');
 const express = require('express');
 const router = express.Router();
 const needle = require('needle');
-const apicache = require('apicache');
 
 const API_BASE_URL = process.env.API_BASE_URL ?? '';
 const API_KEY_NAME = process.env.API_KEY_NAME ?? '';
 const API_KEY_VALUE = process.env.API_KEY_VALUE ?? '';
 
 // Cache settings
-let cache = apicache.middleware;
 
-router.get('/', cache('2 minutes'), async (req, res) => {
+router.get('/', async (req, res) => {
   try {
     const params = new URLSearchParams({
       [API_KEY_NAME]: API_KEY_VALUE,
