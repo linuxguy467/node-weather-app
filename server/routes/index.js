1b23dd62c44965ce50714a58e2d16fd23d6b1715
linuxguy467 <me@matthem.codes>
2021-11-22 14:09:23 -0500

    add redis client

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,12 +2,14 @@ const url = require('url');
 const express = require('express');
 const router = express.Router();
 const needle = require('needle');
+const redis = require('redis');
 
 const API_BASE_URL = process.env.API_BASE_URL ?? '';
 const API_KEY_NAME = process.env.API_KEY_NAME ?? '';
 const API_KEY_VALUE = process.env.API_KEY_VALUE ?? '';
 
 // Cache settings
+const client = redis.createClient(process.env.REDIS_URL);
 
 router.get('/', async (req, res) => {
   try {
