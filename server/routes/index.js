4146484e662f3f142de841ececa4fd572e046fc2
linuxguy467 <me@matthem.codes>
2021-11-22 16:55:26 -0500

    implement redis caching

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,28 +7,47 @@ const redis = require('redis');
 const API_BASE_URL = process.env.API_BASE_URL ?? '';
 const API_KEY_NAME = process.env.API_KEY_NAME ?? '';
 const API_KEY_VALUE = process.env.API_KEY_VALUE ?? '';
+const REDIS_URL = process.env.REDIS_URL ?? '';
+const REDIS_EXPIRY_TIME = process.env.REDIS_EXPIRY_TIME ?? 300;
 
 // Cache settings
-const client = redis.createClient(process.env.REDIS_URL);
+const client = redis.createClient(REDIS_URL);
 
-router.get('/', async (req, res) => {
-  try {
-    const params = new URLSearchParams({
-      [API_KEY_NAME]: API_KEY_VALUE,
-      ...url.parse(req.url, true).query,
-    });
+router.get('/', (req, res) => {
+  const key = `/api?q=${req.query.q}`;
 
-    const apiRes = await needle('get', `${API_BASE_URL}?${params}`);
-    const data = apiRes.body;
-
-    if (process.env.NODE_ENV !== 'production') {
-      console.log(`REQUEST: ${API_BASE_URL}?${params}`);
+  client.get(key, async (err, data) => {
+    if (err) {
+      res.status(500).json(err);
     }
+    if (data) {
+      // send the data from cache
+      return res.status(200).json(JSON.parse(data));
+    } else {
+      try {
+        const params = new URLSearchParams({
+          [API_KEY_NAME]: API_KEY_VALUE,
+          ...url.parse(req.url, true).query,
+        });
+        // request data from open weather map
+        const apiRes = await needle('get', `${API_BASE_URL}?${params}`);
+        const data = apiRes.body;
 
-    res.status(200).json(data);
-  } catch (error) {
-    res.status(500).json(error);
-  }
+        if (process.env.NODE_ENV !== 'production') {
+          console.log(`REQUEST: ${API_BASE_URL}?${params}`);
+          console.log(`Sending requested data to ${REDIS_URL}...`);
+        }
+
+        // cache into Redis
+        client.setex(key, REDIS_EXPIRY_TIME, JSON.stringify(data));
+
+        // send data to client
+        res.status(200).json(data);
+      } catch (error) {
+        res.status(500).json(error);
+      }
+    }
+  });
 });
 
 module.exports = router;
