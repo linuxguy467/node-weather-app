7f93c5f9ce3ca5ad75c87f756c6fcb2dc443cc2e
linuxguy467 <me@matthem.codes>
2021-12-15 14:08:08 -0500

    Add HTML sanitization for weather data

--- server/index.js
+++ server/index.js
@@ -1,37 +1,45 @@
-const express = require('express');
-const cors = require('cors');
-const rateLimit = require('express-rate-limit');
-require('dotenv').config();
+const express = require('express')
+const path = require('path')
+const cors = require('cors')
+const rateLimit = require('express-rate-limit')
+require('dotenv').config()
 
-const app = express();
+const app = express()
 
-const RATE_LIMIT_WINDOW = process.env.RATE_LIMIT_WINDOW ?? 0;
-const RATE_LIMIT_MAX = process.env.RATE_LIMIT_MAX ?? 0;
+const RATE_LIMIT_WINDOW = process.env.RATE_LIMIT_WINDOW ?? 0
+const RATE_LIMIT_MAX = process.env.RATE_LIMIT_MAX ?? 0
 
 // Rate limiting
 const limiter = rateLimit({
   windowMs: RATE_LIMIT_WINDOW,
   max: RATE_LIMIT_MAX,
-});
-app.use(limiter);
-app.set('trust proxy', 1);
+})
+app.use(limiter)
+app.set('trust proxy', 1)
 
-// Static folder
+// Static resources
+const cacheRules = {
+  cacheControl: true,
+  maxAge: 86400000, // 1 day
+}
+
 app.use(
-  express.static('./public', {
-    cacheControl: true,
-    maxAge: 86400000, // 1 day
-  })
-);
+  '/purify.min.js',
+  express.static(
+    path.join(__dirname, '../', 'node_modules/dompurify/dist/purify.min.js'),
+    cacheRules
+  )
+)
+app.use(express.static('./public', cacheRules))
 
 // Routes
-app.use('/api', require('./routes'));
+app.use('/api', require('./routes'))
 
 // Enable cors
-app.use(cors());
+app.use(cors())
 
-const PORT = process.env.PORT ?? 3000;
+const PORT = process.env.PORT ?? 3000
 
 app.listen(PORT, () =>
   console.log(`Server running on http://localhost:${PORT}`)
-);
+)
